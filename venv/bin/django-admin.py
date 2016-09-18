ff690d7498ff7d16a5f858ccd1eab0d05fed9da5
Sam Close <sclose86@gmail.com>
2016-09-18 20:31:28 +0100

    Uploaded template, created polls directory, updated views.py, created polls/urls.py, input code into mysite/urls.py

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/samclose/Documents/Computing_primary/projects/mysite/venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
