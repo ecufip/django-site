fa88accea22b8aac702cc2766bf00e6b74bb3d38
Sam Close <sclose86@gmail.com>
2016-10-07 12:37:01 +0100

    added rendered template to polls/view.py (p3)

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,29 @@
+#from django.http import Http404
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Question
 
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+'''
+This was how it was before I changed it to the render() shortcut:
+
+from django.template import loader
+
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  template = loader.get_template('polls/index.html')
+  context = {
+    'latest_question_list': latest_question_list,
+  }
+  return HttpResponse(template.render(context, request))
+'''
+#These are still in the old format (i.e. not changed to render). If this is changed can delete the HttpResponse import (above)
 def detail(request, question_id):
   return HttpResponse("You're looking at question %s." % question_id)
 
