4b71ccfdb2c67140b2d9f2c92e9cb61e38bcd747
Sam Close <sclose86@gmail.com>
2016-10-10 10:45:38 +0100

    introduced get_object_or_404 to replace Http404 for detail

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Question
@@ -23,14 +22,11 @@ def index(request):
   }
   return HttpResponse(template.render(context, request))
 '''
-#These are still in the old format (i.e. not changed to render). If this is changed can delete the HttpResponse import (above)
-def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
-  return render(request, 'polls/detail.html', {'question': question})
 
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
+#These are still in the old format (i.e. not changed to render). If this is changed can delete the HttpResponse import (above)
 def results(request, question_id):
   response = "You're looking at the results of question %s."
   return HttpResponse(response % question_id)
